564e313d29b98b56fed33d8e5720bd928da1f1b7
Abram Booth <boothaa@gmail.com>
2022-06-13 11:21:46 -0400

    call init_app in mourningwail task

--- mourningwail/tasks.py
+++ mourningwail/tasks.py
@@ -6,6 +6,7 @@ from django.utils import timezone
 from framework.celery_tasks import app as celery_app
 from mourningwail.reporters import DAILY_REPORTERS
 from scripts.utils import add_file_logger
+from website.app import init_app
 
 
 logger = logging.getLogger(__file__)
@@ -14,6 +15,8 @@ logger = logging.getLogger(__file__)
 @celery_app.task(name='mourningwail.tasks.daily_reporters_go')
 def daily_reporters_go(also_send_to_keen=False):
     add_file_logger(logger, __file__)
+
+    init_app()  # OSF-specific setup
 
     # run all reports for the same yesterday
     yesterday = (timezone.now() - timedelta(days=1)).date()
