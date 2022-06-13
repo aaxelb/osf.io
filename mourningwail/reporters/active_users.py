53f65848753953026f6a99c1632b2fa94b819c8a
Abram Booth <boothaa@gmail.com>
2022-06-13 06:48:59 -0400

    delete-unused

--- mourningwail/reporters/active_users.py
+++
@@ -1,7 +0,0 @@
-from mourningwail.metrics import ActiveUsersReport, PageViewEvent
-from ._base import DailyReporter
-
-
-class ActiveUsersReporter(DailyReporter):
-    def report(self, date):
-        pass  # TODO-quest
