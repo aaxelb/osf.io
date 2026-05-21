572f02027e1beeaa4961a534cec8a90a13bf0f77
abram axel booth <boothaa@gmail.com>
2026-05-21 08:18:06 -0400

    capture notifications in tests

--- conftest.py
+++ conftest.py
@@ -18,6 +18,7 @@ from osf.external.spam import tasks as spam_tasks
 from website import settings as website_settings
 from osf.management.commands.populate_notification_types import populate_notification_types
 from osf import features
+from tests.utils import capture_notifications
 
 
 def pytest_configure(config):
@@ -356,3 +357,9 @@ def mock_gravy_valet_get_verified_links():
 @pytest.fixture(autouse=True)
 def load_notification_types(db, *args, **kwargs):
     populate_notification_types(*args, **kwargs)
+
+
+@pytest.fixture(autouse=True)
+def _auto_capture_notifications():
+    with capture_notifications(allow_none=True):
+        yield
